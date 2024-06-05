268a9b586c3dd5d9b2221e7c6b7a128082104437
Sean Allen <seanallengreenall@live.com>
2024-06-05 15:18:00 +0100

    Setting up layout pages

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,11 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import Dashboard from "./views/Dashboard";
+import Login from "./views/Login";
+import Surveys from "./views/Surveys";
+import Signup from "./views/Signup";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />
-    }
-])
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/surveys",
+        element: <Surveys />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 export default router;
